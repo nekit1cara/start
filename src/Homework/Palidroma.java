7d9bb9eda45dc69cdf5f8eca13baabf5169e1eef
nekit1cara <karanchuk@gmail.com>
2023-03-17 17:30:01 +0200

    First commit

---
+++ src/Homework/Palidroma.java
@@ -0,0 +1,22 @@
+package Homework;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Palidroma {
+    public static void main(String[] args) {
+        try {
+            File myObj = new File("C:\\Games\\start\\src\\Homework\\index.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
