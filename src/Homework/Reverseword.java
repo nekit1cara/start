08161888a55398eaa30ca889845e625cc8a1a665
nekit1cara <karanchuk@gmail.com>
2023-03-17 18:07:17 +0200

    First commit

---
+++ src/Homework/Reverseword.java
@@ -0,0 +1,11 @@
+package Homework;
+
+public class Reverseword {
+    public static void main(String[] args) {
+        String word = "обезьяна";
+        String reverse = new StringBuilder(word).reverse().toString();
+        System.out.println(reverse);
+    }
+    // я знаю что мы это не изучали ,
+    // но я к сожалению искал в гугле и нашем метод с помошью StringBuilder.
+}
